fa35f4656c1abce72d94a7e863be7a6ba761bd85
salty-flower <95509524+salty-flower@users.noreply.github.com>
2023-05-24 22:32:24 +0800

    feat: `ArrivalEvent`

---
+++ ValueObject/Snapshot/ArrivalEvent.cs
@@ -0,0 +1,6 @@
+﻿using NobUS.DataContract.Entity;
+
+namespace NobUS.DataContract.ValueObject.Snapshot
+{
+  public sealed partial record ArrivalEvent(Station Station, ShuttleJob ShuttleJob, TimeSpan EstimatedArrivalSpan, DateTime CurrentTime);
+}
