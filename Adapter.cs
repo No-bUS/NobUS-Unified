4eeae11853c448c79b2e229a4e14bc735fe723e9
salty-flower <95509524+salty-flower@users.noreply.github.com>
2023-06-06 21:02:27 +0800

    feat: add congested client

---
+++ Adapter.cs
@@ -0,0 +1,25 @@
+﻿using NobUS.DataContract.Model.Entity;
+using NobUS.DataContract.Model.ValueObject;
+using NobUS.DataContract.Model.ValueObject.Snapshot;
+using NobUS.DataContract.Reader.OfficialAPI.Client;
+
+namespace NobUS.DataContract.Reader.OfficialAPI
+{
+  internal static class Adapter
+  {
+    internal static Route AdaptRoute(ServiceDescription rawRouteMetadata, IEnumerable<Pickuppoint> rawRouteStops) =>
+      new(rawRouteMetadata.Route, rawRouteStops.Select(AdaptStation).ToArray());
+
+    internal static Station AdaptStation(Busstops rawStation) =>
+      new(rawStation.Name, rawStation.Caption, new Coordinate(rawStation.Latitude, rawStation.Longitude));
+
+    internal static Station AdaptStation(Pickuppoint rawStation) =>
+      new(rawStation.Busstopcode, rawStation.Pickupname, new Coordinate(rawStation.Lng, rawStation.Lat));
+
+    internal static ArrivalEvent AdaptArrivalEvent(Station station, _etas rawEta, Dictionary<int, ShuttleJob> shuttleJobDictionary) => 
+      new(station, shuttleJobDictionary[rawEta.Jobid], new TimeSpan(0, 0, rawEta.Eta_s), DateTime.Now);
+
+    internal static MassPoint AdaptMassPoint(Activebus rawMassPoint) =>
+      new(new Coordinate(rawMassPoint.Lng, rawMassPoint.Lat), new Velocity(rawMassPoint.Speed, rawMassPoint.Direction));
+  }
+}
---
+++ CongestedClient.cs
@@ -0,0 +1,131 @@
+﻿using NobUS.DataContract.Model.Entity;
+using NobUS.DataContract.Model.ValueObject.Snapshot;
+using NobUS.DataContract.Reader.OfficialAPI.Client;
+using System.Collections.Immutable;
+
+namespace NobUS.DataContract.Reader.OfficialAPI;
+
+public class CongestedClient
+{
+  public CongestedClient(SchemaClient? client, HttpClient? httpClient)
+  {
+    _client = client ?? new SchemaClient(httpClient ?? new HttpClient());
+    InitStations();
+    InitRoutes();
+    InitVehicles();
+    foreach (var route in _routeSet)
+    {
+      InitShuttleJobs(route);
+    }
+  }
+
+  private readonly SchemaClient _client;
+  private readonly Dictionary<string, Route> _routes = new();
+  private readonly HashSet<Route> _routeSet = new();
+  private readonly Dictionary<string, Station> _stations = new();
+  private readonly HashSet<Station> _stationSet = new();
+  private readonly Dictionary<int, ShuttleJob> _shuttleJobs = new();
+  private readonly HashSet<ShuttleJob> _shuttleJobSet = new();
+  private readonly Dictionary<string, Vehicle> _vehicles = new();
+  private readonly HashSet<Vehicle> _vehicleSet = new();
+
+  private void InitStations()
+  {
+    var rawStations = _client.GetListOfBusStopsAsync();
+    ICollection<Busstops> stations = rawStations.Result.BusStopsResult.Busstops;
+    foreach (var station in stations)
+    {
+      var adaptedStation = Adapter.AdaptStation(station);
+      _stations.Add(station.Name, adaptedStation);
+      _stationSet.Add(adaptedStation);
+    }
+  }
+
+  private void InitRoutes()
+  {
+    var rawRoutes = _client.GetServiceDescriptionAsync();
+    ICollection<ServiceDescription> routes = rawRoutes.Result.ServiceDescriptionResult.ServiceDescription;
+    foreach (var route in routes)
+    {
+      var rawRouteStops = _client.PickupPointAsync(route.Route);
+      var adaptedRoute = Adapter.AdaptRoute(route, rawRouteStops.Result.PickupPointResult.Pickuppoint);
+      _routes.Add(route.Route, adaptedRoute);
+      _routeSet.Add(adaptedRoute);
+    }
+  }
+
+  private void InitVehicles()
+  {
+    IEnumerable<string> StationToPlatesConverter(Station station) =>
+      _client.GetShuttleServiceAsync(station.Name)
+        .Result.ShuttleServiceResult.Shuttles.SelectMany(ss => ss._etas)
+        .Select(eta => eta.Plate);
+
+    var runningVehicles = _stationSet.Select(StationToPlatesConverter).SelectMany(p => p)
+      .Select(p => new Vehicle(null, p));
+    foreach (var runningVehicle in runningVehicles)
+    {
+      _vehicles.Add(runningVehicle.Plate, runningVehicle);
+      _vehicleSet.Add(runningVehicle);
+    }
+  }
+
+  public void UpdateVehicleLocations()
+  {
+    foreach (var route in _routeSet)
+    {
+      var activeBuses = _client.GetActiveBusAsync(route.Name).Result.ActiveBusResult.Activebus;
+      foreach (var activeBus in activeBuses)
+      {
+        var vehicle = _vehicles[activeBus.Vehplate];
+        var updatedVehicle = vehicle with { MassPoint = Adapter.AdaptMassPoint(activeBus) };
+        _vehicles[activeBus.Vehplate] = updatedVehicle;
+        _vehicleSet.Remove(vehicle);
+        _vehicleSet.Add(updatedVehicle);
+      }
+    }
+  }
+
+  private void InitShuttleJobs(Route route)
+  {
+    var rawShuttleJobs = _client.GetActiveBusAsync(route.Name);
+    var rawOngoingShuttleJobs = _stationSet.Select(station =>
+        _client.GetShuttleServiceAsync(station.Name).Result.ShuttleServiceResult.Shuttles.SelectMany(ss => ss._etas))
+      .SelectMany(e => e);
+    ICollection<Activebus> shuttleJobs = rawShuttleJobs.Result.ActiveBusResult.Activebus;
+    foreach (var shuttleJob in shuttleJobs)
+    {
+      var adaptedShuttleJob = new ShuttleJob(rawOngoingShuttleJobs.First(e => e.Plate == shuttleJob.Vehplate).Jobid,
+        route, _vehicles[shuttleJob.Vehplate]);
+      _shuttleJobs.Add(adaptedShuttleJob.Id, adaptedShuttleJob);
+      _shuttleJobSet.Add(adaptedShuttleJob);
+    }
+  }
+
+  public IEnumerable<Station> GetStations() => _stationSet.ToImmutableList();
+  public IEnumerable<Route> GetRoutes() => _routeSet.ToImmutableList();
+
+  public IEnumerable<Vehicle> GetVehicles()
+  {
+    UpdateVehicleLocations();
+    return _vehicleSet.ToImmutableList();
+  }
+
+  public IEnumerable<ShuttleJob> GetShuttleJobs() => _shuttleJobSet.ToImmutableList();
+  public ImmutableDictionary<string, Station> GetStationsDictionary() => _stations.ToImmutableDictionary();
+  public ImmutableDictionary<string, Route> GetRoutesDictionary() => _routes.ToImmutableDictionary();
+
+  public ImmutableDictionary<string, Vehicle> GetVehiclesDictionary()
+  {
+    UpdateVehicleLocations();
+    return _vehicles.ToImmutableDictionary();
+  }
+
+  public ImmutableDictionary<int, ShuttleJob> GetShuttleJobsDictionary() => _shuttleJobs.ToImmutableDictionary();
+
+  public IEnumerable<Station> GetStations(Route route) => route.Stations;
+
+  public IEnumerable<ArrivalEvent> GetArrivalEvents(Station station)
+    => _client.GetShuttleServiceAsync(station.Name).Result.ShuttleServiceResult.Shuttles.SelectMany(ss => ss._etas)
+      .Select(eta => Adapter.AdaptArrivalEvent(station, eta, _shuttleJobs));
+}
