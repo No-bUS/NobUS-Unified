bfb9d53fc04fdf04a2f865e9cedf6ae1db733414
salty-flower <95509524+salty-flower@users.noreply.github.com>
2023-07-09 22:45:19 +0800

    feat: add ToString on Coordinate

--- ValueObject/Coordinate.cs
+++ ValueObject/Coordinate.cs
@@ -1,4 +1,7 @@
 namespace NobUS.DataContract.Model.ValueObject
 {
-  public sealed partial record Coordinate(double Longitude, double Latitude);
+    public sealed partial record Coordinate(double Longitude, double Latitude)
+    {
+        public override string ToString() => $"({Longitude}, {Latitude})";
+    }
 }
