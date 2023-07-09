0e05498274b60ff852de6a1420f413ec5c18bc4a
salty-flower <95509524+salty-flower@users.noreply.github.com>
2023-07-09 22:45:44 +0800

    feat: add DistanceTo to Coordinate

--- ValueObject/Coordinate.cs
+++ ValueObject/Coordinate.cs
@@ -3,5 +3,16 @@ namespace NobUS.DataContract.Model.ValueObject
     public sealed partial record Coordinate(double Longitude, double Latitude)
     {
         public override string ToString() => $"({Longitude}, {Latitude})";
+
+        /// <summary>
+        /// Credit: https://stackoverflow.com/a/60899418
+        /// </summary>
+        /// <returns>Distance in Km</returns>
+        public double DistanceTo(Coordinate other)
+        {
+            double rad(double angle) => angle * 0.017453292519943295769236907684886127d; // = angle * Math.Pi / 180.0d
+            double sinSquareHalf(double diff) => Math.Pow(Math.Sin(rad(diff) / 2d), 2); // = sin²(diff / 2)
+            return 12745.6 * Math.Asin(Math.Sqrt(sinSquareHalf(Latitude - other.Latitude) + Math.Cos(rad(Latitude)) * Math.Cos(rad(other.Latitude)) * sinSquareHalf(other.Longitude - Longitude))); // earth radius 6.372,8Km x 2 = 12745.6
+        }
     }
 }
