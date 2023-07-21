6bc15ea7e4d88a4534dd74f9dba5c23f6b2115e3
salty-flower <95509524+salty-flower@users.noreply.github.com>
2023-07-22 00:50:20 +0800

    style: remove redundant public method modifier

--- IClient.cs
+++ IClient.cs
@@ -1,23 +1,19 @@
-﻿using NobUS.DataContract.Model.Entity;
+﻿using System.Collections.Immutable;
+using NobUS.DataContract.Model.Entity;
 using NobUS.DataContract.Model.ValueObject.Snapshot;
-using NobUS.DataContract.Reader.OfficialAPI.Client;
-using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NobUS.DataContract.Reader.OfficialAPI
 {
-  public interface IClient
-  {
-        public Task<IImmutableList<Station>> GetStationsAsync();
-        public Task<IImmutableList<Route>> GetRoutesAsync();
-        public Task<IImmutableList<Vehicle>> GetVehiclesAsync();
-        public Task<IImmutableList<ShuttleJob>> GetShuttleJobsAsync();
-        public Task<IImmutableList<Station>> GetStationsAsync(Route route);
-        public Task<IImmutableList<ArrivalEvent>> GetArrivalEventsAsync(Station station);
-        public Task<IImmutableList<T>> GetAsync<T>() where T : class;
-  }
+    public interface IClient
+    {
+        Task<IImmutableList<Station>> GetStationsAsync();
+        Task<IImmutableList<Route>> GetRoutesAsync();
+        Task<IImmutableList<Vehicle>> GetVehiclesAsync();
+        Task<IImmutableList<ShuttleJob>> GetShuttleJobsAsync();
+        Task<IImmutableList<Station>> GetStationsAsync(Route route);
+        Task<IImmutableList<ArrivalEvent>> GetArrivalEventsAsync(Station station);
+        Task<IImmutableList<TResult>> GetAsync<TResult>() where TResult : class;
+        Task<IImmutableList<TResult>> GetAsync<TResult, TQuery>(TQuery query)
+            where TResult : class where TQuery : class;
+    }
 }
