55059b6db1312662c4694d54b89dbdad15c7d075
salty-flower <95509524+salty-flower@users.noreply.github.com>
2023-10-08 21:07:06 +0800

    chore: move in Station and Route definitions

---
+++ Service/DefinitionLoader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Collections.Concurrent;
+using System.Collections.Immutable;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Resources = NobUS.Frontend.MAUI.Resources.Definitions.Resources;
+using NobUS.DataContract.Model;
+
+namespace NobUS.Frontend.MAUI.Service
+{
+    [JsonObject]
+    internal record QueryNameMap(string QueryName, int Code);
+
+    internal static class DefinitionLoader
+    {
+        internal static readonly ReadOnlyDictionary<int, string> QueryNameMapping = JsonConvert
+            .DeserializeObject<QueryNameMap[]>(Resources.Definitions.Resources.NUS_Mapping)!
+            .ToDictionary(m => m.Code, m => m.QueryName)
+            .AsReadOnly();
+        private static readonly ReadOnlyDictionary<string, Station> Stations = JsonConvert
+            .DeserializeObject<Station[]>(Resources.Definitions.Resources.NUS_Stations)!
+            .Concat(
+                JsonConvert.DeserializeObject<Station[]>(
+                    Resources.Definitions.Resources.Public_Stations
+                )!
+            )
+            .Where(s => QueryNameMapping.ContainsKey(s.Code))
+            .ToDictionary(s => s.Name)
+            .AsReadOnly();
+        private static readonly ReadOnlyDictionary<string, Route> Routes = JsonConvert
+            .DeserializeObject<Route[]>(Resources.Definitions.Resources.NUS_Routes)!
+            .ToDictionary(r => r.Name)
+            .AsReadOnly();
+
+        public static IImmutableList<Station> GetAllStations => Stations.Values.ToImmutableList();
+
+        public static IImmutableList<Route> GetAllRoutes => Routes.Values.ToImmutableList();
+
+        public static Route GetRouteByName(string name) => Routes[name];
+
+        public static Station GetStationByCode(int code) =>
+            Stations.Values.FirstOrDefault(s => s.Code == code);
+    }
+}
