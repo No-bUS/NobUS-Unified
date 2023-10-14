f43ba5c7b0a12c7cd1323857ca272c089a97f5d1
salty-flower <95509524+salty-flower@users.noreply.github.com>
2023-10-14 22:58:53 +0800

    chore: use `MauiReactor` from first page

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,9 +1,17 @@
-﻿using CommunityToolkit.Maui;
+﻿using Autofac;
+using Autofac.Extras.CommonServiceLocator;
+using CommonServiceLocator;
+using CommunityToolkit.Maui;
 using Material.Components.Maui.Extensions;
 using MaterialColorUtilities.Maui;
 using MauiReactor;
 using Microsoft.Extensions.Logging;
+using NobUS.DataContract.Model;
+using NobUS.DataContract.Reader.OfficialAPI;
+using NobUS.Frontend.MAUI.Presentation;
 using NobUS.Frontend.MAUI.Presentation.View;
+using NobUS.Frontend.MAUI.Repository;
+using NobUS.Frontend.MAUI.Service;
 
 namespace NobUS.Frontend.MAUI
 {
@@ -16,7 +24,34 @@ namespace NobUS.Frontend.MAUI
                 .ToList();
             var builder = MauiApp.CreateBuilder();
             builder
-                .UseMauiApp<App>()
+                .UseMauiReactorApp<PageContainer>(
+                    (app) =>
+                    {
+                        IMaterialColorService.Current.Initialize(app.Resources);
+                        var autofacContainerBuilder = new ContainerBuilder();
+                        autofacContainerBuilder
+                            .RegisterType<CongestedClient>()
+                            .As<IClient>()
+                            .SingleInstance();
+                        autofacContainerBuilder
+                            .RegisterType<LocationProvider>()
+                            .As<ILocationProvider>()
+                            .SingleInstance();
+                        autofacContainerBuilder
+                            .RegisterType<StaticRepository<Station>>()
+                            .As<IRepository<Station>>()
+                            .SingleInstance();
+                        autofacContainerBuilder
+                            .RegisterType<StaticRepository<Route>>()
+                            .As<IRepository<Route>>()
+                            .SingleInstance();
+
+                        var container = autofacContainerBuilder.Build();
+                        ServiceLocator.SetLocatorProvider(
+                            () => new AutofacServiceLocator(container)
+                        );
+                    }
+                )
                 .UseMauiCommunityToolkit()
 #if DEBUG
                 .EnableMauiReactorHotReload()
